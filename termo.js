5885f6f28a7cc7f336870618ac4491dea6aea945
pedrodumbledore <108952479+pedrodumbledore@users.noreply.github.com>
2023-02-01 11:52:37 -0300

    Update termo.js

--- termo.js
+++ termo.js
@@ -8,7 +8,6 @@ function CriaTab(){
             if(c==0){
                 td.style.border="2px solid black";
                 td.style.background="transparent";
-                td.setAttribute("onclick","pos=["+c+","+i+"];Seleciona();");
                 if(i==0){
                     td.style.borderBottom="5px solid black";
                 }
@@ -30,13 +29,20 @@ function CriaTeclado(){
                 img.style.width="10px";
                 img.style.height="10px";
                 input.appendChild(img);
-                input.setAttribute("onclick","TiraLetra()");
+                input.setAttribute("onclick","if(pos[1]>0){TiraLetra();}");
             }
             else{
                 texto=document.createTextNode(letras[c][i]);
                 input.appendChild(texto);
                 input.setAttribute("onclick","ColocaLetra('"+letras[c][i]+"')");
             }
+            div.appendChild(input);
+        }
+        if(c==2){
+            input=document.createElement("button");
+            texto=document.createTextNode("ENTER");
+            input.appendChild(texto);
+            input.setAttribute("onclick","if(Posicao()==4){Testa()};");
             div.appendChild(input);
         }
         br=document.createElement("br");
@@ -47,6 +53,7 @@ function CriaTeclado(){
 function ColocaLetra(letra){
     td=document.getElementById(""+pos[0]+pos[1]);
     if(td.style.borderBottom=="5px solid black"){
+        console.log("entrou");
         td.innerHTML=letra;
         td.style.borderBottom="2px solid black";
         if(pos[1]<4){
@@ -56,14 +63,100 @@ function ColocaLetra(letra){
         }
     }
 }
-function Seleciona(){
-    td=document.getElementById(""+pos[0]+pos[1]);
+function Posicao(){
     for(c=0;c<5;c++){
-        td1=document.getElementById(""+pos[0]+c);
-        td1.style.borderBottom="2px solid black";
+        if(document.getElementById(""+pos[0]+c).innerHTML==""){
+            break
+        }
     }
-    td.style.borderBottom="5px solid black";
+    return c-1;
 }
-var pos=[0,0];
+function TiraLetra(){
+    posi=Posicao();
+    td=document.getElementById(""+pos[0]+posi);
+    td.innerHTML="";
+    td.style.borderBottom="5px solid black";
+    td=document.getElementById(""+pos[0]+(posi+1));
+    td.style.borderBottom="2px solid black";
+    pos[1]--;
+    console.log(pos);
+}
+function SorteiaPalavra(){
+    n=Math.floor(Math.random()*palavras.length);
+    palavra=palavras[n].toUpperCase();
+    console.log(palavra);
+}
+function TestaPalavra(){
+    tof=true;
+    for(c=0;c<5;c++){
+        td=document.getElementById(""+pos[0]+c);
+        if(td.style.backgroundColor!="green"){
+            tof=false
+        }
+    }
+    return tof;
+}
+function Testa(){
+    for(c=0;c<5;c++){
+        td=document.getElementById(""+pos[0]+c)
+        if(td.innerHTML==palavra[c]){
+            td.style="";
+            td.style.backgroundColor="green";
+            palavra=palavra.substring(0,c)+td.innerHTML.toLowerCase()+palavra.substring(c+1);
+            console.log(palavra);
+        }
+        else{
+            td.style="";
+        }
+    }
+    for(c=0;c<5;c++){
+        td=document.getElementById(""+pos[0]+c)
+        if(palavra.indexOf(td.innerHTML)!=-1){
+            td.style="";
+            td.style.backgroundColor="yellow";
+            palavra=palavra.substring(0,c)+palavra[c].toLowerCase()+palavra.substring(c+1);
+            console.log(palavra);
+        }
+    }
+    if(TestaPalavra()==true){
+        tela=[
+            '<div class="container">',
+            '    <div class="box">',
+            '        <h1>Meus Parabéns! Você ganhou!</h1>',
+            '        <a href="index.html"><button class="botao2">Jogar Novamente</button></a>',
+            '    <div>',
+            '</div>'
+        ]
+        setTimeout(()=>{document.querySelector("body").innerHTML=tela.join("\n")},500);
+    }
+    else if(pos[0]==5){
+        tela=[
+            '<div class="container">',
+            '    <div class="box">',
+            '        <h1>Sinto muito! Você perdeu!</h1>',
+            '        <a href="index.html"><button class="botao2">Tentar Novamente</button></a>',
+            '    <div>',
+            '</div>'
+        ]
+        setTimeout(()=>{document.querySelector("body").innerHTML=tela.join("\n")},500);
+    }
+    else{
+        palavra=palavra.toUpperCase();
+        pos=[pos[0]+1,0];
+        for(c=0;c<5;c++){
+            td=document.getElementById(""+pos[0]+c);
+            td.style.border="2px solid black";
+            td.style.background="transparent";
+            if(c==0){
+                td.style.borderBottom="5px solid black";
+            }
+        }
+        
+    }
+}
+var pos=[0,0], palavra;
+var palavras=["carro","comer","fazer","fruta","termo","livro","tabua","porta","lapis","viver","boiar","lindo","farto","parto","sorte","casar","jogar","bolsa","manto","carta","cinto"];
 CriaTab();
 CriaTeclado();
+SorteiaPalavra();
+
